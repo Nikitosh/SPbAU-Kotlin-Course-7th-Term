6c03c10952cb062b4b919f68b99463ba52414b7e
Nikitosh <npodguzov@yandex.ru>
2017-10-25 21:20:51 +0300

    Add first homework

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,14 +1,82 @@
 package ru.spbau.mit
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    
-    words.add("world!")
+import java.io.InputStream
+import java.util.*
 
-    return words.joinToString(separator = " ")
+class Vertex {
+    val neighbours: MutableList<Vertex> = mutableListOf()
+
+    fun addNeighbour(vertex: Vertex) {
+        neighbours.add(vertex)
+    }
+}
+
+class Graph(internal val vertices: List<Vertex>) {
+    fun solve(): List<Int> {
+        val cycleVertices = findCycle()
+        return vertices.map({ vertex -> getDistanceToCycle(vertex, vertex, cycleVertices)!! }).toList()
+    }
+
+    companion object {
+        fun read(inputStream: InputStream): Graph {
+            val input = Scanner(inputStream)
+            val vertexNumber = input.nextInt()
+            val vertices = List(vertexNumber, { _ -> Vertex() })
+            val graph = Graph(vertices)
+            for (i in 1..vertexNumber) {
+                val vertexV = input.nextInt()
+                val vertexU = input.nextInt()
+                graph.addUndirectedEdge(vertices[vertexV - 1], vertices[vertexU - 1])
+            }
+            return graph
+        }
+    }
+
+    internal fun addUndirectedEdge(v: Vertex, u: Vertex) {
+        v.addNeighbour(u)
+        u.addNeighbour(v)
+    }
+
+    internal fun findCycle(): Set<Vertex> {
+        return findCycle(vertices[0], vertices[0], HashSet(), mutableListOf())!!
+    }
+
+    private fun findCycle(vertex: Vertex, previousVertex: Vertex, visitedVertices: MutableSet<Vertex>,
+                          path: MutableList<Vertex>): Set<Vertex>? {
+        visitedVertices.add(vertex)
+        path.add(vertex)
+        vertex.neighbours.forEach({ neighbour ->
+            if (visitedVertices.contains(neighbour) && neighbour != previousVertex) {
+                return path.drop(path.indexOf(neighbour)).toSet()
+            }
+            if (!visitedVertices.contains(neighbour)) {
+                val cycle = findCycle(neighbour, vertex, visitedVertices, path)
+                if (cycle != null) {
+                    return cycle
+                }
+            }
+        })
+        path.removeAt(path.size - 1)
+        return null
+    }
+
+    private fun getDistanceToCycle(vertex: Vertex, previousVertex: Vertex, cycleVertices: Set<Vertex>): Int? {
+        if (cycleVertices.contains(vertex)) {
+            return 0
+        }
+        vertex.neighbours.forEach({ neighbour ->
+            if (neighbour != previousVertex) {
+                val distance = getDistanceToCycle(neighbour, vertex, cycleVertices)
+                if (distance != null) {
+                    return distance + 1
+                }
+            }
+        })
+        return null
+    }
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val graph = Graph.read(System.`in`)
+    println(graph.solve().joinToString(separator = " "))
 }
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -1,10 +1,58 @@
 package ru.spbau.mit
-import kotlin.test.assertEquals
 import org.junit.Test
+import java.io.FileInputStream
+import kotlin.test.assertEquals
 
-class TestSource {
+class TestGraph {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testRead() {
+        testEquals(createSampleGraph1(), Graph.read(FileInputStream("src/test/resources/sample1.txt")))
+        testEquals(createSampleGraph2(), Graph.read(FileInputStream("src/test/resources/sample2.txt")))
+    }
+
+    @Test
+    fun testFindCycle() {
+        assertEquals(4, createSampleGraph1().findCycle().size)
+        assertEquals(3, createSampleGraph2().findCycle().size)
+    }
+
+    @Test
+    fun testSolve() {
+        assertEquals(listOf(0, 0, 0, 0), createSampleGraph1().solve())
+        assertEquals(listOf(0, 0, 0, 1, 1, 2), createSampleGraph2().solve())
+    }
+
+    private fun createSampleGraph1(): Graph {
+        val vertices = List(4, { _ -> Vertex()})
+        val graph = Graph(vertices)
+        graph.addUndirectedEdge(vertices[0], vertices[2])
+        graph.addUndirectedEdge(vertices[3], vertices[2])
+        graph.addUndirectedEdge(vertices[3], vertices[1])
+        graph.addUndirectedEdge(vertices[0], vertices[1])
+        return graph
+    }
+
+    private fun createSampleGraph2(): Graph {
+        val vertices = List(6, { _ -> Vertex()})
+        val graph = Graph(vertices)
+        graph.addUndirectedEdge(vertices[0], vertices[1])
+        graph.addUndirectedEdge(vertices[2], vertices[3])
+        graph.addUndirectedEdge(vertices[5], vertices[3])
+        graph.addUndirectedEdge(vertices[1], vertices[2])
+        graph.addUndirectedEdge(vertices[0], vertices[2])
+        graph.addUndirectedEdge(vertices[2], vertices[4])
+        return graph
+    }
+
+    private fun testEquals(graph1: Graph, graph2: Graph) {
+        assertEquals(graph1.vertices.size, graph2.vertices.size)
+        for (i in 0 until graph1.vertices.size) {
+            val neighbours1 = graph1.vertices[i].neighbours
+            val neighbours2 = graph2.vertices[i].neighbours
+            assertEquals(neighbours1.size, neighbours2.size)
+            for (j in 0 until neighbours1.size) {
+                assertEquals(graph1.vertices.indexOf(neighbours1[j]), graph2.vertices.indexOf(neighbours2[j]))
+            }
+        }
     }
 }

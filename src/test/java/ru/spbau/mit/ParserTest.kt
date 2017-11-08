0bc463c501b0fe012ee73e9c119baf8930e410ea
Nikitosh <npodguzov@yandex.ru>
2017-11-09 01:03:33 +0300

    Add priorities test for parser

--- src/test/java/ru/spbau/mit/ParserTest.kt
+++ src/test/java/ru/spbau/mit/ParserTest.kt
@@ -10,6 +10,45 @@ import kotlin.test.assertEquals
 
 class ParserTest {
     @Test
+    fun testPriorities() {
+        val parser = FunParser(BufferedTokenStream(FunLexer(CharStreams.fromString("1+2*3>5"))))
+        val expectedParseTree =
+"""File
+  Block
+    Statement
+      Expression
+        OrExpression
+          AndExpression
+            EqualityExpression
+              RelationalExpression
+                RelationalExpression
+                  AdditiveExpression
+                    AdditiveExpression
+                      MultiplicativeExpression
+                        UnaryExpression
+                          Literal
+                            [1]
+                    [+]
+                    MultiplicativeExpression
+                      MultiplicativeExpression
+                        UnaryExpression
+                          Literal
+                            [2]
+                      [*]
+                      UnaryExpression
+                        Literal
+                          [3]
+                [>]
+                AdditiveExpression
+                  MultiplicativeExpression
+                    UnaryExpression
+                      Literal
+                        [5]
+"""
+        assertEquals(expectedParseTree, toParseTree(parser.file()))
+    }
+
+    @Test
     fun testExample1() {
         val parser = FunParser(BufferedTokenStream(FunLexer(CharStreams.fromFileName(EXAMPLE1_FILE_NAME))))
         assertEquals(File(EXAMPLE1_PARSE_TREE_FILE_NAME).readText(), toParseTree(parser.file()))
